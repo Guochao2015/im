69cc4f11c2bdd3fc4c7ff3bc8b655afd8babe0df
993605195@qq.com <124146589zy>
2018-02-02 16:00:27 +0800

    链通了

--- Server/src/main/java/org/im/handler/ChannelHandler.java
+++ Server/src/main/java/org/im/handler/ChannelHandler.java
@@ -1,10 +1,11 @@
 package org.im.handler;
 
+import io.netty.channel.ChannelHandlerContext;
 import org.im.exception.PacketException;
 import org.im.exception.UnauthorizedException;
 import org.xmpp.packet.Packet;
 
 public interface ChannelHandler<T extends Packet>  {
 
-    abstract void process(T packet) throws UnauthorizedException, PacketException;
+    abstract void process(T packet, ChannelHandlerContext ctx) throws UnauthorizedException, PacketException;
 }
