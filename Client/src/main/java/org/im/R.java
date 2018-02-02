f52a4e92d5ece6fac0875db9edc82f7077fd0a5a
993605195@qq.com <124146589zy>
2018-02-02 15:59:13 +0800

    链通了

---
+++ Client/src/main/java/org/im/R.java
@@ -0,0 +1,12 @@
+package org.im;
+
+public class R {
+
+    public static class id{
+        public static final String LOGIN = "Login";
+    }
+    public static class Layout{
+        public static final String LOGIN = "fxml/login/login.fxml";
+    }
+
+}
